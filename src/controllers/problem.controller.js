d69d74bbc26fb84843536fec6aee05d18c20d2ef
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-05 17:58:26 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,21 +3,31 @@ export const pingProblemController = (req, res) =>{
 }
 
 export const addProblem = (req, res)=>{
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 export const getProblem = (req, res) =>{
-
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 export const getProblems = (req, res) => {
-    
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 export const deleteProblem = (req,res) => {
-    
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 export const updateProblem = (req, res) => {
-    
+    return res.status(501).json({
+        message: 'Not implemented'
+    });
 }
