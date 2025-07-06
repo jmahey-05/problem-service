22007f26bc464d58ca2c60f1da18de9f4d4ab70a
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-07 02:09:57 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { PORT } from './config/server.config.js';
 import { apiRouter } from './routes/index.js';
 import { errorHandler } from './utils/errorHandler.js';
+import { connectToDB } from './config/db.config.js';
 
 const app = express();
 
@@ -18,7 +19,8 @@ app.get('/ping',(req,res) =>{
 
 // last middleware if any error comes
 app.use(errorHandler);
-app.listen(PORT,()=>{
+app.listen(PORT,async ()=>{
     console.log(`Server started at PORT: ${PORT}`)
-
+    await connectToDB();
+    console.log("Successfully connected to db")
 })
